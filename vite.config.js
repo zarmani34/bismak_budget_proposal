2f0a756a7eea78097de8f1943f8c4150ef600b14
[Abdulazeez Idowu] <[idowuabdulazeez39@gmail.com]>
2025-11-18 12:09:23 +0100

    comment out base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/bismak_budget_proposal/',
+  // base: '/bismak_budget_proposal/', //Comment out base for vercel hosting but uncomment if you want to push to GhPages
 })
