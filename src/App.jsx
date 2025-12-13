c128e61a07ab6636c03e7d4a0d195e70333a865b
[Abdulazeez Idowu] <[idowuabdulazeez39@gmail.com]>
2025-12-13 22:12:25 +0100

    Fix case-sensitive rename for MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import Proposal from "./components/Proposal";
 import Budget from "./components/Budget";
-import Mainlayout from "./layouts/Mainlayout";
+import MainLayout from "./layouts/MainLayout";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-import { Router } from "lucide-react";
 
 function App() {
   const [count, setCount] = useState(0);
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Mainlayout />}>
+      <Route path="/" element={<MainLayout />}>
         <Route index element={<Proposal />} />
         <Route path="budget" element={<Budget />} />
       </Route>
